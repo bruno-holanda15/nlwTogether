35d957eb659a2a71a45d09a5a0109b4441f26c71
Bruno Holanda <br.holanda15@gmail.com>
2021-06-25 19:24:44 -0300

    Created list of send and receive compliments from user

--- src/controllers/CreateTagController.ts
+++ src/controllers/CreateTagController.ts
@@ -1,8 +1,6 @@
 import { Request , Response } from "express";
 import { CreateTagService } from "../services/CreateTagService";
 
-
-
 class CreateTagController{
     async handle(request: Request, response: Response) {
         const { name } = request.body;
